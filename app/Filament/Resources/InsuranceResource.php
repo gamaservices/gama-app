2f5f61533ca498ba4ee12be865a7cd2db3a6d4fc
joselomanuelo <joselomanuelo@gmail.com>
2024-01-13 13:39:42 -0500

    feat: insurance resource

---
+++ app/Filament/Resources/InsuranceResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\InsuranceResource\Pages;
+use App\Models\Insurance;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class InsuranceResource extends Resource
+{
+    protected static ?string $model = Insurance::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('property_id')
+                    ->relationship('property', 'id')
+                    ->required(),
+                Forms\Components\TextInput::make('policy_number')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('type')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('company')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DateTimePicker::make('start_at')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('expired_at')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('property.id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('policy_number')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('type')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('company')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('start_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('expired_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListInsurances::route('/'),
+            'create' => Pages\CreateInsurance::route('/create'),
+            'edit' => Pages\EditInsurance::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/InsuranceResource/Pages/CreateInsurance.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\InsuranceResource\Pages;
+
+use App\Filament\Resources\InsuranceResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateInsurance extends CreateRecord
+{
+    protected static string $resource = InsuranceResource::class;
+}
