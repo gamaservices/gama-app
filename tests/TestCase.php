7b37136752d0afca0b62372b10238c084bc6f23b
joselomanuelo <joselomanuelo@gmail.com>
2024-01-18 18:26:09 -0500

    feat: test initial setting

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,37 @@
 
 namespace Tests;
 
+use App\Models\City;
+use App\Models\NotaryOffice;
+use App\Models\State;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected State $state;
+
+    protected City $city;
+
+    protected NotaryOffice $notaryOffice;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->state = State::factory()->create();
+
+        $this->city = City::factory()
+            ->for($this->state)
+            ->create();
+
+        $this->notaryOffice = NotaryOffice::factory()
+            ->for($this->state)
+            ->for($this->city)
+            ->create();
+
+        $this->actingAs(User::factory()->create());
+    }
 }
