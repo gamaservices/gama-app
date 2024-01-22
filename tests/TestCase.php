fb7b0f45bdf32c8879ece2dd861ad0cfa287d3aa
joselomanuelo <joselomanuelo@gmail.com>
2024-01-22 10:08:29 -0500

    feat: add city and state in testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace Tests;
 
 use App\Models\City;
-use App\Models\NotaryOffice;
 use App\Models\State;
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
@@ -16,11 +15,12 @@ abstract class TestCase extends BaseTestCase
 
     protected City $city;
 
-    protected NotaryOffice $notaryOffice;
-
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->state = State::factory()->create();
+        $this->city = City::factory()->for($this->state)->create();
 
         $this->actingAs(User::factory()->create());
     }
