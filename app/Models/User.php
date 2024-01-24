08e54d4df0fa2640a3765fe768358ede8cc27a62
joselomanuelo <joselomanuelo@gmail.com>
2024-01-24 16:40:31 -0500

    feat: filament shield scaffolding without test fixing

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
     use HasFactory;
+    use HasRoles;
     use Notifiable;
 
     /**
