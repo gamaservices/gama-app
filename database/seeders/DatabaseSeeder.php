8efc1da9155e1cbcbfe1c71f308a09a9a030e93d
joselomanuelo <joselomanuelo@gmail.com>
2024-01-25 15:14:36 -0500

    feat: make master a super admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,19 +2,21 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()
+        $master = User::factory()
             ->create([
                 'name'  => 'Master',
                 'email' => 'master@gama.services',
             ]);
+
+        Artisan::call('shield:super-admin --user=' . $master->id);
 
         User::factory()
             ->create([
