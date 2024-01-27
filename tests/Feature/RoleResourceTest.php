8aa71897f0b3fd43a047a7338e588045683a2fd1
joselomanuelo <joselomanuelo@gmail.com>
2024-01-27 15:07:43 -0500

    test: Role resource basic tests

--- tests/Feature/RoleResourceTest.php
+++ tests/Feature/RoleResourceTest.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\Shield\RoleResource\Pages\EditRole;
 use App\Filament\Resources\Shield\RoleResource\Pages\ListRoles;
 use Filament\Actions\DeleteAction;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 use function Pest\Livewire\livewire;
@@ -19,134 +20,126 @@ it('can list roles', function () {
         ->assertCanSeeTableRecords(Role::all());
 });
 
-it('can render create page', function () {
+it('can render create page', closure: function () {
     $this->get(RoleResource::getUrl('create'))->assertSuccessful();
 });
 
 it('can create a role', function () {
-    $newData = Role::factory()->make();
+    $newData = Role::create(['name' => 'testing-role']);
 
     livewire(CreateRole::class)
         ->fillForm([
-            'name'                  => $newData->name,
-            'email'                 => $newData->email,
-            'password'              => 'new_password',
-            'password_confirmation' => 'new_password',
-            'roles'                 => $this->role->id,
+            'name'       => $newData->name . '_role',
+            'guard_name' => $newData->guard_name,
+            'insurance'  => ['view_any_insurance'],
         ])
         ->call('create')
         ->assertHasNoFormErrors();
 
-    $user = Role::where('name', $newData->name)->first();
-
-    $this->assertTrue(Hash::check('new_password', $user->password));
-
     $this->assertDatabaseHas(Role::class, [
-        'name'  => $newData->name,
-        'email' => $newData->email,
+        'name'       => $newData->name . '_role',
+        'guard_name' => $newData->guard_name,
     ]);
 
-    $this->assertDatabaseHas('model_has_roles', [
-        'role_id'  => $this->role->id,
-        'model_id' => $user->id,
+    $permission = Permission::findByName('view_any_insurance');
+
+    $this->assertDatabaseHas('role_has_permissions', [
+        'permission_id' => $permission->id,
+        'role_id'       => $newData->id + 1,
     ]);
 });
 
 it('can validate create input', function () {
     livewire(CreateRole::class)
         ->fillForm([
-            'name'                  => null,
-            'email'                 => null,
-            'password'              => null,
-            'password_confirmation' => 'other',
-            'roles'                 => null,
+            'name' => null,
         ])
         ->call('create')
         ->assertHasFormErrors([
-            'name'                  => 'required',
-            'email'                 => 'required',
-            'password'              => 'required',
-            'password_confirmation' => 'same:password',
-            'roles'                 => 'required',
+            'name' => 'required',
+        ]);
+
+    $newData = Role::create(['name' => 'testing-role']);
+
+    livewire(CreateRole::class)
+        ->fillForm([
+            'name' => $newData->name,
+        ])
+        ->call('create')
+        ->assertHasFormErrors([
+            'name' => 'unique',
         ]);
 });
 
 it('can render edit page', function () {
     $this->get(RoleResource::getUrl('edit', [
-        'record' => Role::factory()->create(),
+        'record' => Role::create(['name' => 'testing-role']),
     ]))->assertSuccessful();
 });
 
 it('can retrieve data', function () {
-    $user = Role::factory()->create();
+    $role       = Role::create(['name' => 'testing-role']);
+    $permission = Permission::create(['name' => 'view_any_insurance']);
+    $role->givePermissionTo($permission);
 
     livewire(EditRole::class, [
-        'record' => $user->getRouteKey(),
+        'record' => $role->getRouteKey(),
     ])
         ->assertFormSet([
-            'name'                  => $user->name,
-            'email'                 => $user->email,
-            'password'              => null,
-            'password_confirmation' => null,
-            'roles'                 => $user->roles->pluck('id')->toArray(),
+            'name'       => $role->name,
+            'guard_name' => $role->guard_name,
+            'insurance'  => ['view_any_insurance'],
         ]);
 });
 
-it('can save an user', function () {
-    $user = Role::factory()->create();
+it('can save a role', function () {
+    $role = Role::create(['name' => 'testing-role-1']);
 
-    $newData = Role::factory()->make();
+    $newData = Role::create(['name' => 'testing-role-2']);
 
     livewire(EditRole::class, [
-        'record' => $user->getRouteKey(),
+        'record' => $role->getRouteKey(),
     ])
         ->fillForm([
-            'name'                  => $newData->name,
-            'email'                 => $newData->email,
-            'password'              => 'new_password',
-            'password_confirmation' => 'new_password',
-            'roles'                 => $this->role->id,
+            'name'       => $newData->name . '_role',
+            'guard_name' => $newData->guard_name,
+            'insurance'  => ['view_any_insurance'],
         ])
         ->call('save')
         ->assertHasNoFormErrors();
 
-    expect($user->refresh())
-        ->name->toBe($newData->name)
-        ->email->toBe($newData->email)
-        ->password->not()->toBeNull()
-        ->roles->first()->id->toBe($this->role->id);
+    expect($role->refresh())
+        ->name->toBe($newData->name . '_role')
+        ->guard_name->toBe($newData->guard_name);
+
+    $this->assertDatabaseHas('role_has_permissions', [
+        'permission_id' => Permission::findByName('view_any_insurance')->id,
+        'role_id'       => $role->id,
+    ]);
 });
 
 it('can validate edit input', function () {
-    $Role = Role::factory()->create();
+    $role = Role::create(['name' => 'testing-role']);
 
     livewire(EditRole::class, [
-        'record' => $Role->getRouteKey(),
+        'record' => $role->getRouteKey(),
     ])
         ->fillForm([
-            'name'                  => null,
-            'email'                 => null,
-            'password'              => null,
-            'password_confirmation' => 'other',
-            'roles'                 => null,
+            'name' => null,
         ])
         ->call('save')
         ->assertHasFormErrors([
-            'name'                  => 'required',
-            'email'                 => 'required',
-            'password'              => 'required',
-            'password_confirmation' => 'same:password',
-            'roles'                 => 'required',
+            'name' => 'required',
         ]);
 });
 
-it('can delete an user', function () {
-    $Role = Role::factory()->create();
+it('can delete a role', function () {
+    $role = Role::create(['name' => 'testing-role']);
 
     livewire(EditRole::class, [
-        'record' => $Role->getRouteKey(),
+        'record' => $role->getRouteKey(),
     ])
         ->callAction(DeleteAction::class);
 
-    $this->assertModelMissing($Role);
+    $this->assertModelMissing($role);
 });
