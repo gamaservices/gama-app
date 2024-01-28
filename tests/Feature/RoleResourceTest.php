edb82431ca3cdaa7168f70f5fd33a5d2ae580030
joselomanuelo <joselomanuelo@gmail.com>
2024-01-28 13:48:37 -0500

    test: role resource test completion

--- tests/Feature/RoleResourceTest.php
+++ tests/Feature/RoleResourceTest.php
@@ -11,44 +11,82 @@ use Spatie\Permission\Models\Role;
 use function Pest\Livewire\livewire;
 
 it('can render list page', function () {
-    $this->get(RoleResource::getUrl())->assertSuccessful();
+    $this->get(RoleResource::getUrl())
+        ->assertSuccessful();
+
+    $this->assertAuthenticated();
+});
+
+it('cannot render list page when user do not have permission', function () {
+    $this->actingAs($this->user)
+        ->get(RoleResource::getUrl())
+        ->assertForbidden();
+
+    $this->assertAuthenticated();
 });
 
 it('can list roles', function () {
     livewire(ListRoles::class)
-        ->assertCanSeeTableRecords(Role::all());
+        ->assertCanSeeTableRecords(Role::all())
+        ->assertCountTableRecords(2)
+        ->assertCanRenderTableColumn('name')
+        ->assertCanRenderTableColumn('guard_name')
+        ->assertCanRenderTableColumn('permissions_count')
+        ->assertCanRenderTableColumn('updated_at')
+        ->searchTable('super_admin')
+        ->assertCanSeeTableRecords(Role::where('name', 'super_admin')->get());
+
+    $this->assertAuthenticated();
 });
 
 it('can render create page', closure: function () {
-    $this->get(RoleResource::getUrl('create'))->assertSuccessful();
+    $this->get(RoleResource::getUrl('create'))
+        ->assertSuccessful();
+
+    $this->assertAuthenticated();
+});
+
+it('cannot render create page when user do not have permission', function () {
+    $this->actingAs($this->user)
+        ->get(RoleResource::getUrl('create'))
+        ->assertForbidden();
+
+    $this->assertAuthenticated();
 });
 
 it('can create a role', function () {
-    $newData = Role::create(['name' => 'testing-role']);
-
     livewire(CreateRole::class)
+        ->assertFormExists()
+        ->assertFormFieldExists('name')
+        ->assertFormFieldExists('guard_name')
+        ->assertFormFieldExists('user')
+        ->assertFormFieldExists('property')
+        ->assertFormFieldExists('role')
         ->fillForm([
-            'name'       => $newData->name . '_role',
-            'guard_name' => $newData->guard_name,
-            'insurance'  => ['view_any_insurance'],
+            'name'       => 'testing_role',
+            'guard_name' => 'web',
+            'property'   => ['view_any_property'],
         ])
         ->call('create')
         ->assertHasNoFormErrors();
 
     $this->assertDatabaseHas(Role::class, [
-        'name'       => $newData->name . '_role',
-        'guard_name' => $newData->guard_name,
+        'name'       => 'testing_role',
+        'guard_name' => 'web',
     ]);
 
-    $permission = Permission::findByName('view_any_insurance');
+    $role       = Role::findByName('testing_role');
+    $permission = Permission::findByName('view_any_property');
 
     $this->assertDatabaseHas('role_has_permissions', [
         'permission_id' => $permission->id,
-        'role_id'       => $newData->id + 1,
+        'role_id'       => $role->id,
     ]);
 });
 
 it('can validate create input', function () {
+    $role = Role::create(['name' => 'testing-role']);
+
     livewire(CreateRole::class)
         ->fillForm([
             'name' => null,
@@ -56,17 +94,22 @@ it('can validate create input', function () {
         ->call('create')
         ->assertHasFormErrors([
             'name' => 'required',
-        ]);
-
-    $newData = Role::create(['name' => 'testing-role']);
-
-    livewire(CreateRole::class)
+        ])
         ->fillForm([
-            'name' => $newData->name,
+            'name' => $role->name,
         ])
         ->call('create')
         ->assertHasFormErrors([
             'name' => 'unique',
+        ])
+        ->fillForm([
+            'name'       => str_repeat('a', 256),
+            'guard_name' => str_repeat('a', 256),
+        ])
+        ->call('create')
+        ->assertHasFormErrors([
+            'name'       => 'max:255',
+            'guard_name' => 'max:255',
         ]);
 });
 
@@ -74,45 +117,60 @@ it('can render edit page', function () {
     $this->get(RoleResource::getUrl('edit', [
         'record' => Role::create(['name' => 'testing-role']),
     ]))->assertSuccessful();
+
+    $this->assertAuthenticated();
+});
+
+it('cannot render edit page when user do not have permission', function () {
+    $this->actingAs($this->user)
+        ->get(RoleResource::getUrl('edit', [
+            'record' => Role::create(['name' => 'testing-role']),
+        ]))->assertForbidden();
+
+    $this->assertAuthenticated();
 });
 
 it('can retrieve data', function () {
-    $role       = Role::create(['name' => 'testing-role']);
-    $permission = Permission::create(['name' => 'view_any_insurance']);
+    $role       = Role::create(['name' => 'testing_role']);
+    $permission = Permission::create(['name' => 'view_any_property']);
     $role->givePermissionTo($permission);
 
     livewire(EditRole::class, [
         'record' => $role->getRouteKey(),
     ])
+        ->assertFormExists()
+        ->assertFormFieldExists('name')
+        ->assertFormFieldExists('guard_name')
+        ->assertFormFieldExists('user')
+        ->assertFormFieldExists('property')
+        ->assertFormFieldExists('role')
         ->assertFormSet([
             'name'       => $role->name,
             'guard_name' => $role->guard_name,
-            'insurance'  => ['view_any_insurance'],
+            'property'   => ['view_any_property'],
         ]);
 });
 
 it('can save a role', function () {
-    $role = Role::create(['name' => 'testing-role-1']);
-
-    $newData = Role::create(['name' => 'testing-role-2']);
+    $role = Role::create(['name' => 'testing_role']);
 
     livewire(EditRole::class, [
         'record' => $role->getRouteKey(),
     ])
         ->fillForm([
-            'name'       => $newData->name . '_role',
-            'guard_name' => $newData->guard_name,
-            'insurance'  => ['view_any_insurance'],
+            'name'       => 'another_testing_role',
+            'guard_name' => 'web',
+            'property'   => ['view_any_property'],
         ])
         ->call('save')
         ->assertHasNoFormErrors();
 
     expect($role->refresh())
-        ->name->toBe($newData->name . '_role')
-        ->guard_name->toBe($newData->guard_name);
+        ->name->toBe('another_testing_role')
+        ->guard_name->toBe('web');
 
     $this->assertDatabaseHas('role_has_permissions', [
-        'permission_id' => Permission::findByName('view_any_insurance')->id,
+        'permission_id' => Permission::findByName('view_any_property')->id,
         'role_id'       => $role->id,
     ]);
 });
@@ -129,6 +187,15 @@ it('can validate edit input', function () {
         ->call('save')
         ->assertHasFormErrors([
             'name' => 'required',
+        ])
+        ->fillForm([
+            'name'       => str_repeat('a', 256),
+            'guard_name' => str_repeat('a', 256),
+        ])
+        ->call('save')
+        ->assertHasFormErrors([
+            'name'       => 'max:255',
+            'guard_name' => 'max:255',
         ]);
 });
 
