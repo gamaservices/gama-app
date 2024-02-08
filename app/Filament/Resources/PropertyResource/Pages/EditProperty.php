3631e56c059bc0da2bd93d8a817bc87cdd47eef7
joselomanuelo <joselomanuelo@gmail.com>
2024-02-08 15:09:36 -0500

    feat: property resource logger

--- app/Filament/Resources/PropertyResource/Pages/EditProperty.php
+++ app/Filament/Resources/PropertyResource/Pages/EditProperty.php
@@ -5,9 +5,12 @@ namespace App\Filament\Resources\PropertyResource\Pages;
 use App\Filament\Resources\PropertyResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Noxo\FilamentActivityLog\Extensions\LogEditRecord;
 
 class EditProperty extends EditRecord
 {
+    use LogEditRecord;
+
     protected static string $resource = PropertyResource::class;
 
     protected function getHeaderActions(): array
